d75218e35ceff84620c99013703c1746d82e6122
0XAymax <aymanderrouich3@gmail.com>
2025-05-24 13:13:18 +0100

    Adding router

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import App from './App.tsx'
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-)
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import router from "./router"; 
+import { AuthProvider } from "./context/AuthContext";
+import { Toaster } from 'sonner';
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <Toaster richColors/>
+       <RouterProvider router={router} />
+    </AuthProvider>
+  </React.StrictMode>
+);
