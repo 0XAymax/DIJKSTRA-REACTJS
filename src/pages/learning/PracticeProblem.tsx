e03f8475dbbf55af26b5a8d9f4c1a6f3ec262954
0XAymax <aymanderrouich3@gmail.com>
2025-05-30 20:22:56 +0100

    adding mcq component

--- src/pages/learning/PracticeProblem.tsx
+++ src/pages/learning/PracticeProblem.tsx
@@ -1,18 +1,28 @@
 import type { PracticeProblems } from "@/types";
 import "@/markdown.css";
-import "@/components/ui/mcq.css";
 import MCQComponent from "./MCQ";
+import NotFound from "./notFound";
+import InteractiveGraph from "./InteractiveGraph";
 
 function PracticeProblem({ problem }: { problem: PracticeProblems }) {
+  if (!problem) {
+    return <NotFound />
+  }
   const parsedData = JSON.parse(problem?.data || "{}");
   const choices = parsedData.choices || [];
   const answer = parsedData.answer || 0;
   const question = problem?.question || "";
- 
+  const Problemtype = problem?.type;
 
   return (
+    <>
+      {Problemtype === "multiple_choice" && (
         <MCQComponent question={question} choices={choices} correctIndex={answer} />
+      )}
+      {Problemtype === "interactive_graph" && <InteractiveGraph />}
+    </>
   );
+
 }
 
 export default PracticeProblem;
