22dedeaec82337145ff6fefcdbc5bb9f26faf776
0XAymax <aymanderrouich3@gmail.com>
2025-06-01 20:31:25 +0100

    trying again

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,6 @@ export default defineConfig({
     },
   },
   build: {
-    outDir: 'build'  // or 'build' or any folder Vercel expects
+    outDir: 'dist'  // or 'build' or any folder Vercel expects
   }
 });
